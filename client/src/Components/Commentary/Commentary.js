a73fb6cec0260f1b56ba47a0a2627dd0d2738e4e
Thibb1 <thibault.brumaire@gmail.com>
2023-02-12 19:06:20 +0100

    + profile picture

--- client/src/Components/Commentary/Commentary.js
+++ client/src/Components/Commentary/Commentary.js
@@ -5,6 +5,8 @@ export default function Commentary({cardData}) {
   return (
     <div className="commentary">
       <p>Username: {cardData.user.name}</p>
+      <img src={cardData.user.picture.replace("https", "http")} alt={cardData.user.name}
+      style={{width: "50px", height: "50px", borderRadius: "50%"}}/>
       <p>Comment: {cardData.text}</p>
     </div>
   );
