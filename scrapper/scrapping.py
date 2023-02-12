3362ef9756735971f61dec45e797385d1d2c19ac
Curtis <curtis1337wastaken@gmail.com>
2023-02-12 15:30:36 +0100

    ADD : Scrapped review

--- scrapper/scrapping.py
+++ scrapper/scrapping.py
@@ -2,13 +2,37 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import os
+import random
 
 def get_html(start):
     html = requests.get(f"https://www.imdb.com/search/title/?title_type=feature&num_votes=3000,&keywords=superhero&sort=user_rating,desc&count=100&start={start}&ref_=adv_nxt").text
     return BeautifulSoup(html, 'html.parser')
 
 
+def get_reviews(reference):
+    res = requests.get(f"https://www.imdb.com/title/{reference}/reviews?ref_=tt_urv").text
+    rev_html = BeautifulSoup(res, 'html.parser')
+    count = random.randint(8, 13)
+    flag = 0
+    reviews = {}
+    author_list = []
+    for a in rev_html.find("div", {"class": "lister-list"}).find_all("div"):
+        try:
+            author = a.find("span", {"class": "display-name-link"}).text
+            if author not in author_list:
+                review = a.find("div", {"class": "content"}).find("div", {"class" : "text"}).text
+                reviews[flag] = f"{author} :: {review}"
+                if flag > count:
+                    return reviews
+                flag+=1
+                print(flag)
+                author_list.append(author)
+        except:
+            continue
+    return reviews
+
 def parse_data(html_parsed, count):
+    print("Scrapping Started")
     all_data = []
     list = html_parsed.find("div", {"class": "lister-list"})
     for movie in list.find_all("div"):
@@ -23,6 +47,8 @@ def parse_data(html_parsed, count):
             indexStar = str(movie).find("Star")
             Stars = ""
             Directors = ""
+            ref = movie.find("img", {"class": "loadlate"}, alt=True)['data-tconst'].strip()
+            reviews = get_reviews(ref)
             for a in names_div:
                 if a['href'].startswith("/name/"):
                     if(str(movie).find(a.text) < indexStar):
@@ -30,7 +56,6 @@ def parse_data(html_parsed, count):
                     else:
                         Stars += f"{a.text} "
             dict_movie = {
-                "id" : count,
                 "certificate": certificate,
                 "runtime": runtime,
                 "genre": genre,
@@ -38,7 +63,8 @@ def parse_data(html_parsed, count):
                 "rating": rating,
                 "year": year,
                 "Directors": Directors,
-                "Stars": Stars
+                "Stars": Stars,
+                "Reviews": reviews
             }
             all_data.append(dict_movie)
             count += 1
