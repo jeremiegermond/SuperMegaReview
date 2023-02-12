c76c2e798c80e252f8b808b382636bad033ee4fb
Jeremie <jeremie@localhost.localdomain>
2023-02-12 15:55:13 +0100

    add: commentary section

--- client/src/Components/CardDetail/CardDetail.js
+++ client/src/Components/CardDetail/CardDetail.js
@@ -3,6 +3,7 @@ import "./CardDetail.css"
 import cardData from "../../Data/scrapped_data.json"
 import { FaStar } from 'react-icons/fa';
 import React from "react";
+import Commentary from "../Commentary/Commentary";
 
 export default function CardDetail() {
   const { id } = useParams();
@@ -11,27 +12,38 @@ export default function CardDetail() {
 
   return (
     <div>
-      {selectedCard ? (
-        <div className="film-review">
-          <div className="film-review-left">
-            <img src="/batman.jpg" alt={selectedCard.title} />
-            <h1>{selectedCard.title}</h1>
-            <h3>Rating: {selectedCard.rating}<FaStar className="card-star" /></h3>
+      {selectedCard ?
+        <div>
+          <div className="film-review">
+            <div className="film-review-left">
+              <img src="/batman.jpg" alt={selectedCard.title} />
+              <h1>{selectedCard.title}</h1>
+              <h3>Rating: {selectedCard.rating}<FaStar className="card-star" /></h3>
+            </div>
+            <div className="film-review-right">
+              <p className="film-review-subtitle">Certificate</p>
+              <p>{selectedCard.certificate}</p>
+              <p className="film-review-subtitle">Genre</p>
+              <p>{selectedCard.genre}</p>
+              <p className="film-review-subtitle">Time</p>
+              <p>{selectedCard.runtime}</p>
+              <p className="film-review-subtitle">Release Year</p>
+              <p>{selectedCard.year}</p>
+              <p className="film-review-subtitle">Directors</p>
+              <p>{selectedCard.Directors}</p>
+              <p className="film-review-subtitle">Stars</p>
+              <p>{selectedCard.Stars}</p>
+            </div>
           </div>
-          <div className="film-review-right">
-            <p className="film-review-subtitle">Score:</p>
-            <p>8.7</p>
-            <p className="film-review-subtitle">Type de film</p>
-            <p>Science-Fiction</p>
-            <p className="film-review-subtitle">Durée</p>
-            <p>1h57</p>
-            <p className="film-review-subtitle">Réalisateurs</p>
-            <p>Jean Dujardin, Gilles Lelouche</p>
-            <p className="film-review-subtitle">Stars</p>
-            <p>Jean Dujardin, Gilles Lelouche</p>
+          <div className="commentary-page">
+            <h1>Commentaries</h1>
+            <div className="commentary-cards">
+              {cardData.map((card) => (
+                <Commentary key={card.id} cardData={card} />
+              ))}
+            </div>
           </div>
-        </div>
-      ) : (
+        </div> : (
         <p>Card not found</p>
       )}
     </div>
