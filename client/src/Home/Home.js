4cd70182ca6fa028ad22aa49eebf46a555f4b022
jeremiegermond <jeremie.germond@epitech.eu>
2023-02-12 19:55:46 +0100

    add: game presentation

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "./Home.css"
+import { Link } from "react-router-dom"
 
 export default function Home() {
   return (
     <div className="home-container">
       <h1 className="home-title">Checkout this game!</h1>
       <div className="home-mid">
-        <div className="game-image"></div>
+        <Link className="home-image" to="https://github.com/Clement-Lnrd/Garbage-Clicker">
+          <img alt="image" src="/image.png" className="game-image"></img>
+        </Link>
         <div className="game-writing">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>Garbage Clicker is a 2D shooter game whose purpose is to pick up waste bags that pass on the screen.
+            Incarnate several heroes [that are Epitech pedagos] and catch all the garbages!
+          </p>
         </div>
       </div>
     </div>
