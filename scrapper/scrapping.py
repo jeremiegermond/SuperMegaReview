ecd30c7653d9045b9cc6d98b10cd1bfcaef17645
Curtis <curtis1337wastaken@gmail.com>
2023-02-12 16:45:21 +0100

    ADD : Scrapper cover + review formatting

--- scrapper/scrapping.py
+++ scrapper/scrapping.py
@@ -4,6 +4,8 @@ import json
 import os
 import random
 
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
 def get_html(start):
     html = requests.get(f"https://www.imdb.com/search/title/?title_type=feature&num_votes=3000,&keywords=superhero&sort=user_rating,desc&count=100&start={start}&ref_=adv_nxt").text
     return BeautifulSoup(html, 'html.parser')
@@ -21,15 +23,30 @@ def get_reviews(reference):
             author = a.find("span", {"class": "display-name-link"}).text
             if author not in author_list:
                 review = a.find("div", {"class": "content"}).find("div", {"class" : "text"}).text
-                reviews[flag] = f"{author} :: {review}"
+                reviews[flag] = {
+                   "id": flag,
+                   "author": author,
+                   "review": review 
+                }
                 if flag > count:
                     return reviews
                 flag+=1
-                print(flag)
                 author_list.append(author)
         except:
             continue
     return reviews
+
+def get_cover(ref):
+    try:
+        res = requests.get(f"https://www.imdb.com/title/{ref}/?ref_=tt_urv", headers=headers).text
+        disp_html = BeautifulSoup(res, 'html.parser')
+        link = disp_html.find("a", {"class": "ipc-lockup-overlay ipc-focusable"})['href']
+        res_img = requests.get(f"https://www.imdb.com{link}", headers=headers).text
+        img_html = BeautifulSoup(res_img, 'html.parser')
+        cover_link = img_html.find("img", {"class": "sc-7c0a9e7c-0 fEIEer"})['src']
+        return cover_link
+    except:
+        return None
 
 def parse_data(html_parsed, count):
     print("Scrapping Started")
@@ -41,6 +58,7 @@ def parse_data(html_parsed, count):
             runtime = movie.find("span", {"class": "runtime"}).text.strip()
             genre = movie.find("span", {"class": "genre"}).text.strip()
             title = movie.find("img", {"class": "loadlate"}, alt=True)['alt'].strip()
+            print(f"Currently Scrapping : {title}")
             rating = movie.find("div", {"class": "inline-block ratings-imdb-rating"})['data-value'].strip()
             year = movie.find("span", {"class": "lister-item-year text-muted unbold"}).text.strip()[1:-1]
             names_div = movie.find_all("a")
@@ -49,6 +67,8 @@ def parse_data(html_parsed, count):
             Directors = ""
             ref = movie.find("img", {"class": "loadlate"}, alt=True)['data-tconst'].strip()
             reviews = get_reviews(ref)
+            ref = movie.find("img", {"class": "loadlate"}, alt=True)['data-tconst'].strip()
+            cover = get_cover(ref)
             for a in names_div:
                 if a['href'].startswith("/name/"):
                     if(str(movie).find(a.text) < indexStar):
@@ -64,7 +84,8 @@ def parse_data(html_parsed, count):
                 "year": year,
                 "Directors": Directors,
                 "Stars": Stars,
-                "Reviews": reviews
+                "Reviews": reviews,
+                "cover": cover
             }
             all_data.append(dict_movie)
             count += 1
